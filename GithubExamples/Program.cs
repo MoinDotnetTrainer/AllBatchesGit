99d6862284b204d9b569f969eb754e0007cc877e
Admin <onlyonemoin@gmail.com>
2024-01-25 14:33:23 +0530

    Add project files.

---
+++ GithubExamples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            add();
+        }
+
+        public static void add()
+        {
+            int x = 45, y = 56, z;
+            z = x + y;
+            Console.WriteLine("add is :" + z);
+        }
+    }
+}
